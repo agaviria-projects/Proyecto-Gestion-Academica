3f4620e055357fdf84340f2ce068d17f3850faa7
Alejandro <agaviria1408@gmail.com>
2025-04-10 16:08:00 -0500

    Inicio del proyecto integrador: Gestión Académica

---
+++ src/main/java/gestor/gestor_academico/GestorAcademicoApplication.java
@@ -0,0 +1,13 @@
+package gestor.gestor_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorAcademicoApplication.class, args);
+	}
+
+}
