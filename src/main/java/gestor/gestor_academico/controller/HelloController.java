9a3e4cdc706b0b1bc31baa1260538fdbcd0c9831
Alejandro <agaviria1408@gmail.com>
2025-06-07 22:43:45 -0500

    Agrego HelloController para validar respuesta en raíz

---
+++ src/main/java/gestor/gestor_academico/controller/HelloController.java
@@ -0,0 +1,13 @@
+package gestor.gestor_academico.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Servidor backend funcionando correctamente";
+    }
+}
