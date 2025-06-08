a8fa8e83e72ebeaeb444acea3d12c68c7b642a9a
Alejandro <agaviria1408@gmail.com>
2025-06-07 22:56:46 -0500

    Corrijo error de importación y puerto dinámico

--- src/main/java/gestor/gestor_academico/GestorAcademicoApplication.java
+++ src/main/java/gestor/gestor_academico/GestorAcademicoApplication.java
@@ -1,14 +1,15 @@
 package gestor.gestor_academico;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class
-GestorAcademicoApplication {
+public class GestorAcademicoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestorAcademicoApplication.class, args);
+		int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+		new SpringApplicationBuilder(GestorAcademicoApplication.class)
+				.properties("server.port=" + port)
+				.run(args);
 	}
-
 }
