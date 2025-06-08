419f12f5009f38ac044c7a83390959f0b6d5314c
Alejandro <agaviria1408@gmail.com>
2025-06-08 15:23:12 -0500

    Corrijo errores de paquete y finalizo backend listo para Railway

--- src/main/java/gestor/gestor_academico/GestorAcademicoApplication.java
+++ src/main/java/gestor/gestor_academico/GestorAcademicoApplication.java
@@ -2,13 +2,17 @@ package gestor.gestor_academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class GestorAcademicoApplication {
 
 	public static void main(String[] args) {
+		// Para adaptarse al puerto dinámico de Railway
+		String port = System.getenv("PORT");
+		if (port != null) {
+			System.getProperties().put("server.port", port);
+		}
 		SpringApplication.run(GestorAcademicoApplication.class, args);
-
 	}
 }
